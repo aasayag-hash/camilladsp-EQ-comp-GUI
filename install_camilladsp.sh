baf51e61844bf89d57c1253b3ca59c76dba6e424
aasayag-hash <aasayag@gmail.com>
2026-04-11 10:31:27 -0300

    chore: update installation script to improve dependency handling and path configuration

--- install_camilladsp.sh
+++ install_camilladsp.sh
@@ -1,4 +1,4 @@
-##!/usr/bin/env bash
+#!/usr/bin/env bash
 # ==============================================================
 #  CamillaDSP — Instalador Automático
 #  Engine  +  GUI Backend  +  Frontend
